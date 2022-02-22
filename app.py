aab4f5278ab907c9e9b7279a12ab7e104c3a686f
abatgali <abatgali@iu.edu>
2022-02-21 23:59:29 -0500

    logo fix

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+# to disable caching
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
 def index():
 
