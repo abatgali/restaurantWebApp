b81c2c5d19cd3bcc73fc132dec32a6932b3d6cd9
abatgali <abatgali@iu.edu>
2022-03-09 23:54:43 -0500

    signup ✅

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, redirect
-import dbconn
+from flask import Flask, render_template, redirect, request
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+
 
 app = Flask(__name__)
 
@@ -12,24 +14,53 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+
 @app.route("/")
 def index():
 
     return render_template('index.html')
+
 
 @app.route("/menu")
 def menu():
 
     return render_template('menu.html')
 
+
 @app.route("/gift")
 def gift():
 
     return render_template('giftcards.html')
 
-@app.route("/signin")
-def signin():
 
-    return render_template('signin.html')
-
+@app.route("/signin", methods=["POST", "GET"])
+def signup():
+    
+    if request.method == "GET":
+        return render_template('signin.html')
+        
+        
+    if request.method == "POST":
+        
+        # retrieve info from form
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        email = request.form.get('email')
+        
+        # phone = request.form.get('phone')
+        pwd = request.form.get('pwd')
+        c_pwd = request.form.get('c_pwd')
+        
+        
+        if pwd == c_pwd:
+            
+            with sqlite3.connect('restaurant.db', check_same_thread=False) as conn:
+            
+                db = conn.cursor()
+                db.execute('INSERT INTO users (email, hash, name) VALUES (?, ?, ?)', (email, generate_password_hash(pwd), fname + ' ' + lname))
+                #db.commit()
+                
+        return redirect('/menu')
+    
+ 
 app.run()
--- dbconn.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('restaurant.db')
-
-if (conn):
-    print('connection successful')
-else:
-    print('connection failed')
